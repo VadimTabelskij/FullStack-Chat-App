2a9305c4bc9e15ef40a62fd939248c71962f2791
VadimTabelskij <bagum94@gmail.com>
2023-04-25 14:09:16 +0300

    FullStack Chat App Final Project

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
-const mainRouter = require('./router/mainRouter.js');
+const index = require('./router/index.js');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -22,4 +22,4 @@ app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
 
-app.use('/', mainRouter);
+app.use('/', index);
